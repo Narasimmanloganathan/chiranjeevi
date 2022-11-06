01c12a08ef1cf6395e341b4b09934a348f6ac3c8
Narasimmanloganathan <narasimman314@gmail.com>
2022-11-06 07:46:02 -0800

    3

---
+++ javascript/three.js
@@ -0,0 +1,15 @@
+let x=1000;
+let y =70000;
+let z =20000;
+
+if (x>y && x>z)
+    {
+        console.log("the greatest number is:"+x);
+}
+if(y>z && y>x){
+    console.log("the greatest number is:"+y);
+}
+
+if(z>x && z>y){
+    console.log("the greatest number is:"+z);
+}
