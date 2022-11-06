6dd8f50407b376ec71c753f94582c0841896e992
Narasimmanloganathan <narasimman314@gmail.com>
2022-11-06 07:11:27 -0800

    2

--- javascript/one.js
+++ javascript/one.js
@@ -1,7 +1,9 @@
-let number= 15;
+let number= 21;
 
+/*
 if (number%7==0){
     console.log("number is divisible by 7");
 }else{
     console.log("number not divisible by 7");
-}
+}*/
+number%7==0 ? console.log( "number is divisible by 7") : console.log("number not is divisible by 7")
