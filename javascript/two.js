300a2bc16341b2c2900e73f725507421417fd8be
Narasimmanloganathan <narasimman314@gmail.com>
2022-11-06 07:22:05 -0800

    3

---
+++ javascript/two.js
@@ -0,0 +1,9 @@
+let number1= 30;
+let number2 =50;
+/*
+if (number1>number2){
+    console.log("gretest number is:" + number1);
+}else{
+    console.log("gretest number is:" + number2);
+}*/
+number1>number2 ?console.log("gretest number is:" + number1):console.log("gretest number is:" + number2);
