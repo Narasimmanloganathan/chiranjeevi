4ff45e74845660530f4fd6955af6e3988d2f4e73
Narasimmanloganathan <narasimman314@gmail.com>
2022-11-09 20:49:26 +0530

    ok

---
+++ javascript/four.js
@@ -0,0 +1,25 @@
+let x=1000;
+let y=20000;
+let z=5300;
+
+if (x<=y && x<=z){
+    if(y<x){
+        console.log(y, x, z);
+    }else{
+        console.log(x, y, z);
+    }
+}
+if (y<=x && y<=z){
+    if(z<y){
+        console.log(z, y, x);
+    }else{
+        console.log(y, z, x);
+    }
+}
+if (z<=x && z<=y){
+    if(x<z){
+        console.log(x, z, y);
+    }else{
+        console.log(z, x, y);
+    }
+}
